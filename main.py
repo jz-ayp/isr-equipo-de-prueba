26d6858ffb50e3e79ddaccc58cf02ca308f506cc
Jorge Zaldívar <jorge.zaldivar@gmail.com>
2021-03-15 00:34:57 -0600

    Roberto, Alejandra y Juan Pablo Valdez

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-# Fecha: 03/03/2021
-# Autores: Bruno, Maite y Juan Pablo Rubio
+# ISR
+# Equipo 2: Roberto, Alejandra y Juan Pablo Valdez
+# Fecha: 25/02/2021
 
-# ENTRADA
-salario = float(input("Escriba su salario"))
+#Entradas
+salario = float(input("Digite el salario para el calculo del impuesto:"))
 
-#PROCESO
-if salario >= 0.01 and salario <= 3644.94:    
- lim_inferior = 0.01
- lim_superior = 3644.94
- excedente = salario - lim_inferior
- impuesto_marginal = excedente * .10
- isr = impuesto_marginal + 12.88
-elif salario >= 3644.95 and salario <=7446.19:
- lim_inferior = 3644.95
- lim_superior = 7446.19
- excedente = salario - lim_inferior
- impuesto_marginal = excedente * .20
- isr = impuesto_marginal + 303.76
-elif salario >= 7446.20 and salario <=10298.35:
- lim_inferior = 7446.20
- lim_superior = 10298.35
- excedente = salario - lim_inferior
- impuesto_marginal = excedente * .30
- isr = impuesto_marginal + 1063.92
+#Procesos
+if salario <= 3644.94:
+  ISR = 12.88 + ((salario - 0.01) * 0.10)
+  print(ISR)
 
-else:
- lim_inferior = 10298.36
- excedente = salario - lim_inferior
- impuesto_marginal = excedente * .35
- isr = impuesto_marginal + 3327.42
+if  3644.94<=salario <= 7446.19:
+  ISR = 303.76 + ((salario - 3644.95) * 0.20)
+  print(ISR)
 
-#SALIDA
-print("El el impuesto sobre la renta es de", isr)
+if 7446.19<= salario <= 10298.35:
+  ISR = 1063.92 + ((salario - 7446.20) * 0.30)
+  print(ISR)
 
+if salario > 10298.35:
+  ISR = 3327.42 + ((salario - 10298.35) * 0.35)
+  print(ISR)
